7fef5c55c936cb611b09d7fff75a08c1428ef8a6
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-10-20 00:58:42 +0330

    add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'primary': "#FD1A96"
+            }
         },
     },
 

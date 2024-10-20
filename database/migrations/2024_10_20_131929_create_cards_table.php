64aa10bbd81c0ffd90ad3b76e19685ccf1667e59
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-10-20 17:13:24 +0330

    add cards model

---
+++ database/migrations/2024_10_20_131929_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Deck;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Deck::class)->constrained();
+            $table->string('query', 255);
+            $table->string('answer', 255);
+            $table->timestamp('reviewed_at')->nullable();
+            $table->tinyInteger('difficulty', unsigned: true);
+            $table->tinyInteger('failed_reviews', unsigned: true)->default(0);
+            $table->tinyInteger('success_reviews', unsigned: true)->default(0);
+            $table->text('user_notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};

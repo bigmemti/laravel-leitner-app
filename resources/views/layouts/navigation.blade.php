3181d65d868335c37c634b45cec5516619b108dd
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-12-09 17:35:33 +0330

    add deck link in responsive nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,6 +73,10 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            
+            <x-responsive-nav-link :href="route('deck.index')" :active="request()->routeIs('deck.index')">
+                {{ __('Deck') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
